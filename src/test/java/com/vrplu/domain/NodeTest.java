3b23de7052ffe852a507e7caaf9da00d06fc4768
kousik-kr <kousikkumar95@gmail.com>
2025-11-30 22:56:37 +0530

    new feature: upgrade Java version to 21

--- src/test/java/com/vrplu/domain/NodeTest.java
+++ src/test/java/com/vrplu/domain/NodeTest.java
@@ -1,8 +1,8 @@
 package com.vrplu.domain;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests for Node class
